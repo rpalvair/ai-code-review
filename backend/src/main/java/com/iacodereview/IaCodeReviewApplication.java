a7a2054b9ddbf5e87affdc2a2a0ffca3c067b9a2
ruddy palvair <r.palvair@gmail.com>
2026-05-08 17:14:24 +0200

    Fix dependance RestClient.Builder

--- backend/src/main/java/com/iacodereview/IaCodeReviewApplication.java
+++ backend/src/main/java/com/iacodereview/IaCodeReviewApplication.java
@@ -3,11 +3,18 @@ package com.iacodereview;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class IaCodeReviewApplication {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(IaCodeReviewApplication.class, args);
+    }
+
+    @Bean
+    public RestClient.Builder builder() {
+        return RestClient.builder();
     }
 }
